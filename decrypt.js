af2fabcbc03dfa5fafdb91d929c170b087532d5b
brianstevo <57068977+brianstevo@users.noreply.github.com>
2020-04-10 16:38:48 +0530

    Add files via upload
    added decrypt.js

---
+++ decrypt.js
@@ -0,0 +1,18 @@
+const permutation = require('./permutation');
+const pbox = require('./pbox');
+const feistel = require('./feistel');
+
+//This function takes an array of generated keys and 8bit ciphertext
+module.exports = (key, text) =>{
+  //Performing IP
+  let ip = permutation(text, pbox.initial);
+  //First Feistel round
+  let result = feistel(ip, key[1]);
+  //Swapping the results
+  result = permutation(result, pbox.swap);
+  //Second Feistel round
+  result = feistel(result, key[0]);
+  //Performing IP inverse to obtain plaintext
+  let plaintext = permutation(result, pbox.initialInverse);
+  return plaintext;
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+// const encrypt= require('./encrypt');
+// const decrypt= require('./decrypt');
+// const keygen= require('./keyGen');
+
+// let key=keygen('1010000010');
+// let cipher=encrypt(key,'10010111');
+// let plaintext=decrypt(key,cipher);
+// console.log(cipher);
+// console.log(plaintext);
+
