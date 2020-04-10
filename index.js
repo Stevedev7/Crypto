42a2f0865eaa2e7254d8efd6f9328fea7ee6f65d
brianstevo <57068977+brianstevo@users.noreply.github.com>
2020-04-10 17:33:04 +0530

    Add files via upload
    edited index

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const binary = require('./stringToBinary');
-
+const encrypt= require('./encrypt');
+const decrypt= require('./decrypt');
+const keygen= require('./keyGen');
 const string = require('./binaryToString');
 
 
 const inputText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz;"
-
 const bin = binary(inputText);
-
-console.log(bin);
-
-console.log(string(bin));
+let convert="";
+let bits = bin.match(/.{1,8}/g);
+// const bin = binary(inputText);
+let key=keygen('1010000010');
+console.log(bits);
+bits.forEach(bit=>{
+let cipher=encrypt(key,bit);
+let plaintext=decrypt(key,cipher);
+convert=convert+plaintext;
+console.log(cipher+" encrypted");
+console.log(plaintext+" decrypted");
+});
+// console.log(string(bin));
+// console.log(convert);
+console.log(string(convert));
