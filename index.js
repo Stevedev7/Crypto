8270f98cad2757b0fb2b9681592b0dc97a754494
Steve <pinto_steve@yahoo.com>
2020-04-10 19:56:35 +0530

    Minor changes

--- index.js
+++ index.js
@@ -4,28 +4,46 @@ const decrypt= require('./decrypt');
 const keygen= require('./keyGen');
 const string = require('./binaryToString');
 
+//Plaintext and keytext
+const inputText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,/\\=+-_:;\'\"{[(*&^%$#@!~\`<>)]}"
+const keyText = "CrYPtoGRAphy";
 
-const inputText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz;"
-const bin = binary(inputText);
-let decryptedtext="";
-let encryptedtext="";
-let bits = bin.match(/.{1,8}/g);
-// const bin = binary(inputText);
-let key=keygen('1010000010');
-console.log(bits);
-bits.forEach(bit=>{
-let cipher=encrypt(key,bit);
-let plaintext=decrypt(key,cipher);
-encryptedtext=encryptedtext+cipher;
-decryptedtext=decryptedtext+plaintext;
-console.log(cipher+" encrypted");
-// console.log(string(cipher));
-console.log(plaintext+" decrypted");
-// console.log(string(plaintext));
+//converting plaintext and keytext into binary
+const binaryPlaintext = binary(inputText);
+let keyBits = binary(keyText);
+
+//Make the add additions bits to make it long enough
+let flag = true;
+while(keyBits.length % 10 != 0){
+  flag = !flag;
+  flag ? keyBits += '1' : keyBits += '0';
+}
+
+let decryptedText="";
+let encryptedText="";
+let plainTextBytes = binaryPlaintext.match(/.{1,8}/g);
+let keyArray = keyBits.match(/.{1,10}/g);
+
+//Key generation
+var keyPairs = [];
+keyArray.forEach(key => {
+  keyPairs.push(keygen(key));
 });
-// console.log(string(bin));
-// console.log(convert);
-console.log(encryptedtext);
-console.log(string(encryptedtext));
-console.log(decryptedtext);
-console.log(string(decryptedtext));
+let x = '';
+//Encrypting
+for (let i = 0, j = 0; i < plainTextBytes.length; i++, j = ++j % keyArray.length) {
+  let key = keyPairs[j];
+  encryptedText += encrypt(key, plainTextBytes[i]);
+}
+//Printing the result
+console.log(string(encryptedText));
+//Splitting the cipher into bytes
+let cipherBytes = encryptedText.match(/.{1,8}/g);
+
+//Decrypting the cipher
+for (let i = 0, j = 0; i < cipherBytes.length; i++, j = ++j % keyArray.length) {
+  let key = keyPairs[j];
+  decryptedText += decrypt(key, cipherBytes[i]);
+}
+
+console.log(string(decryptedText));
